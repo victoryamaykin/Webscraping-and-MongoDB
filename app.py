982e95f7b3f2124dd598573f954d8eb480db99d9
Victor Yamaykin <victor.yamaykin@gmail.com>
2023-01-19 15:59:22 -0500

    Update app.py
    adding streamlit

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+import streamlit as st
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+import pymongo
 import scrape_mars
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
 
 @app.route("/")
 def echo():
